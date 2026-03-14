9a62fba9627227678f08523175e1f70e43a4346f
adwords711-commits <adwords711@gmail.com>
2026-03-14 14:13:31 -0400

    updated html link and meta content link iwht github account

---
+++ ponder/sortfilter.js
@@ -0,0 +1,25 @@
+nums = [12, 10, 8, 3];
+
+console.log(nums.sort(compareFn));
+
+
+function compareFn(a,b) {
+  if (a < b) {
+    return -1;
+  } else if (a > b) {
+    return 1;
+  }
+ return 0;
+}
+
+const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
+
+let simpleSort = simpleList.sort();
+console.log(simpleSort);
+
+let lowerList = simpleList.map(function(fruit) {
+    return fruit.toLowerCase();
+})
+
+let lowerSort = lowerList.sort();
+console.log(lowerSort);
