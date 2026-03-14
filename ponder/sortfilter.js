c4b63660ed31204e24c5ffb92d53a717cb20580f
adwords711-commits <adwords711@gmail.com>
2026-03-14 18:22:47 -0400

    completed hikes assigment

--- ponder/sortfilter.js
+++ ponder/sortfilter.js
@@ -1,16 +1,9 @@
 nums = [12, 10, 8, 3];
 
+
+
 console.log(nums.sort(compareFn));
 
-
-function compareFn(a,b) {
-  if (a < b) {
-    return -1;
-  } else if (a > b) {
-    return 1;
-  }
- return 0;
-}
 
 const simpleList = ["oranges", "grapes", "lemons", "apples", "Bananas", "watermelons", "coconuts", "broccoli", "mango"];
 
@@ -23,3 +16,79 @@ let lowerList = simpleList.map(function(fruit) {
 
 let lowerSort = lowerList.sort();
 console.log(lowerSort);
+
+let searchTerm = 'b';
+
+let filterFruit = lowerSort.filter(searchFruit);
+
+function searchFruit(item) {      
+    return item.includes(searchTerm);
+}
+
+function compareFn(a,b) {
+  if (a.productName < b.productName) {
+    return -1;
+  } else if (a.price > b.price) {
+    return 1;
+  }
+ return 0;
+}
+
+const products = [
+  {
+    productName: "Wireless Mouse",
+    price: 29.99
+  },
+  {
+    productName: "Bluetooth Keyboard",
+    price: 49.99
+  },
+  {
+    productName: "Laptop Stand",
+    price: 39.99
+  }
+];
+let productSort = products.sort(compareFn);
+console.log(productSort);
+
+
+const animals = [
+  {
+    name: "Lion",
+    traits: ["brave", "strong", "fierce", "wild"]
+  },
+  {
+    name: "Elephant",
+    traits: ["large", "gentle", "smart", "wild"]
+  },
+  {
+    name: "Fox",
+    traits: ["sly", "quick", "clever", "wild"]
+  },
+  {
+    name: "Dog",
+    traits: ["loyal", "friendly", "playful", "cuddly"]
+  },
+  {
+    name: "Cat",
+    traits: ["quiet", "independent", "curious", "cuddly"]
+  }
+];
+
+let query = 'ox';
+let filterList = animals.filter(searchList);
+
+function searchList(item){
+    return item.name.toLowerCase().includes(query.toLowerCase());
+}
+
+console.log(filterList);
+
+let queryTrait = 'cuddly';
+let filteredTraits = animals.filter(searchList);
+
+function searchTraits(item){
+    return item.traits.find((trait) => trait.toLowerCase().includes(queryTrait.toLowerCase()));
+}
+
+console.log(filteredTraits);
